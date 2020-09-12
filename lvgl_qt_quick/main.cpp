c78ffd044e1e7c4d83b629df888e722e00b53996
Jon Povirk <jmp180@pitt.edu>
2020-09-12 18:42:09 -0400

    Added lv_examples

--- lvgl_qt_quick/main.cpp
+++ lvgl_qt_quick/main.cpp
@@ -6,12 +6,11 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QQuickWindow>
+#include <lv_examples/lv_examples.h>
 
 // Local
 #include "LvglContext.hpp"
 #include "LvglImageProvider.hpp"
-
-static void create_demo();
 
 int main(int argc, char* argv[])
 {
@@ -22,7 +21,9 @@ int main(int argc, char* argv[])
   auto                  lvgl_image_provider = std::make_unique<LvglImageProvider>(lvgl);
 
   engine.addImageProvider("LvglImageProvider", lvgl_image_provider.release());  // engine takes ownership
-  create_demo();
+
+  /// lvgl_sim_qt_example
+  lv_demo_benchmark();
 
   const QUrl url(QStringLiteral("qrc:/main.qml"));
   engine.load(url);
@@ -40,22 +41,4 @@ int main(int argc, char* argv[])
   }
 
   return app.exec();
-}
-
-void create_demo()
-{
-  static lv_obj_t* label1 = lv_label_create(lv_scr_act(), NULL);
-  lv_label_set_long_mode(label1, LV_LABEL_LONG_BREAK); /*Break the long lines*/
-  lv_label_set_recolor(label1, true);                  /*Enable re-coloring by commands in the text*/
-  lv_label_set_align(label1, LV_LABEL_ALIGN_CENTER);   /*Center aligned lines*/
-  lv_label_set_text(label1, "#0000ff Re-color# #ff00ff words# #ff0000 of a# label "
-                            "and  wrap long text automatically.");
-  lv_obj_set_width(label1, 150);
-  lv_obj_align(label1, NULL, LV_ALIGN_CENTER, 0, -30);
-
-  static lv_obj_t* label2 = lv_label_create(lv_scr_act(), NULL);
-  lv_label_set_long_mode(label2, LV_LABEL_LONG_SROLL_CIRC); /*Circular scroll*/
-  lv_obj_set_width(label2, 150);
-  lv_label_set_text(label2, "It is a circularly scrolling text. ");
-  lv_obj_align(label2, NULL, LV_ALIGN_CENTER, 0, 30);
 }
--- lvgl_qt_widgets/main.cpp
+++ lvgl_qt_widgets/main.cpp
@@ -1,12 +1,10 @@
 // 3rd Party
 #include <QApplication>
-#include <lvgl/lvgl.h>
+#include <lv_examples/lv_examples.h>
 
 // Local
 #include "LvglContext.hpp"
 #include "MainWindow.hpp"
-
-static void create_demo();
 
 int main(int argc, char* argv[])
 {
@@ -14,26 +12,9 @@ int main(int argc, char* argv[])
   LvglContext  lvgl;
   MainWindow   window(lvgl);
 
-  create_demo();
+  /// lvgl_sim_qt_example
+  lv_demo_benchmark();
 
   window.show();
   return app.exec();
-}
-
-static void create_demo()
-{
-  static lv_obj_t* label1 = lv_label_create(lv_scr_act(), NULL);
-  lv_label_set_long_mode(label1, LV_LABEL_LONG_BREAK); /*Break the long lines*/
-  lv_label_set_recolor(label1, true);                  /*Enable re-coloring by commands in the text*/
-  lv_label_set_align(label1, LV_LABEL_ALIGN_CENTER);   /*Center aligned lines*/
-  lv_label_set_text(label1, "#0000ff Re-color# #ff00ff words# #ff0000 of a# label "
-                            "and  wrap long text automatically.");
-  lv_obj_set_width(label1, 150);
-  lv_obj_align(label1, NULL, LV_ALIGN_CENTER, 0, -30);
-
-  static lv_obj_t* label2 = lv_label_create(lv_scr_act(), NULL);
-  lv_label_set_long_mode(label2, LV_LABEL_LONG_SROLL_CIRC); /*Circular scroll*/
-  lv_obj_set_width(label2, 150);
-  lv_label_set_text(label2, "It is a circularly scrolling text. ");
-  lv_obj_align(label2, NULL, LV_ALIGN_CENTER, 0, 30);
 }
